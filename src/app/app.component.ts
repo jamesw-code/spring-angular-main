fd2e3ceb8ef4fa781c599a64de1562cae5f623d4
James Wyatt <jamesrwyatt35@gmail.com>
2024-09-06 23:21:47 -0400

    refactor away from module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Demo';
+  title = 'Not';
   data = {}  as any;
   constructor(private http: HttpClient) {
     http.get('resource').subscribe(data => this.data = data);
